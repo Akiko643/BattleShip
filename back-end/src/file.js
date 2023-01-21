1030b41614c5b4642180dedacf4dce69b4dc7a21
Akiko <amarjargal.a@steppelink.mn>
2023-01-21 18:20:59 +0800

    generate files

--- back-end/src/file.js
+++ back-end/src/file.js
@@ -3,6 +3,7 @@ import util from "util";
 
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
+const appendFile = util.promisify(fs.appendFile);
 
 export const saveData = async ({ username, map, code }) => {
     const userData = {
@@ -19,4 +20,35 @@ export const saveData = async ({ username, map, code }) => {
     else obj.push(userData);
     const json = JSON.stringify(obj);
     await writeFile(path, json);
+};
+
+export const generateData = async () => {
+    const folder = "./data/";
+    const dataPath = folder + "data.json";
+    const mapPath = folder + "map.txt";
+    const userPath = folder + "user.txt";
+    const codePath = folder + "code.txt";
+
+    const data = await readFile(dataPath);
+    const obj = JSON.parse(data);
+
+    await appendFile(userPath, JSON.stringify(obj.length) + "\n");
+    await appendFile(mapPath, JSON.stringify(obj.length) + "\n");
+    await appendFile(codePath, JSON.stringify(obj.length) + "\n");
+
+    for (let i = 0; i < obj.length; i++) {
+        const { username, map, code } = obj[i];
+        await appendFile(userPath, username + " ");
+        for (let j = 0; j < map.length; j++) {
+            for (let k = 0; k < map[j].length; k++) {
+                await appendFile(mapPath, JSON.stringify(map[j][k]) + " ");
+            }
+        }
+        const fixedCode =
+            "    {\n" +
+            code +
+            "\n\n" +
+            "        shooters.pb(shooter);\n    }\n";
+        await appendFile(codePath, fixedCode);
+    }
 };
